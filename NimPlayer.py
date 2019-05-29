b9e6852418895425b639d6ca29ae11fcf80ce1fd
Peter Hansel <pwhansel@stanford.edu>
2019-05-29 01:37:11 -0700

    4x4 in 2.0155 seconds

--- NimPlayer.py
+++ NimPlayer.py
@@ -8,11 +8,15 @@ from random import *
 STONE_DIAMETER = 40
 STONE_COLOR = "blue"
 SQUARE_WIDTH = STONE_DIAMETER + 10
-NUM_ROWS = 3
+NUM_ROWS = 4
 NUM_COLS = 4
 SCREEN_WIDTH = NUM_COLS * SQUARE_WIDTH + 100
-SCREEN_HEIGHT = NUM_ROWS * SQUARE_WIDTH + 100
+SCREEN_HEIGHT = max(NUM_ROWS * SQUARE_WIDTH + 100, 220)
 COMPUTER_FIRST = True
+
+"""
+graphics handling, etc
+"""
 
 def clickedCircle(circ, click):
 	center = circ.getCenter()
@@ -44,8 +48,165 @@ def createLines(win):
 		line = Line(Point(left, y), Point(right, y))
 		line.draw(win)
 
+def clickedExit(click):
+	return NUM_COLS * SQUARE_WIDTH + 20 <= click.getX() <= NUM_COLS * SQUARE_WIDTH + 80 and 160 <= click.getY() <= 200
+
 def clickedBox(click):
 	return NUM_COLS * SQUARE_WIDTH + 10 <= click.getX() <= NUM_COLS * SQUARE_WIDTH + 90 and 10 <= click.getY() <= 90
+
+"""
+symmetry rules
+"""
+def getRow(indicator, row_to_get):
+	ans = [[col[row_to_get]] for col in indicator]
+
+def getRows(indicator, bot_row):
+	ans = [[col[i] for i in range(bot_row, len(indicator[0]))] for col in indicator]
+	return ans
+
+def biggestRow(indicator):
+	row_lengths = [sum([1 for col in range(indicator.shape[0]) if indicator[col][row] == 1]) for row in range(indicator.shape[1])]
+	# print("biggestRow indicator", indicator)
+	# print("row_lengths", row_lengths)
+	# print("---")
+	max_row = max(row_lengths)
+	ans = [i for i in range(indicator.shape[1]) if row_lengths[i]==max_row][0]
+	# print("ans", ans)
+	return ans
+
+def biggestCol(indicator):
+	# print("biggestCol indicator", indicator)
+	col_lengths = [sum([1 for row in range(indicator.shape[1]) if indicator[col][row] == 1]) for col in range(indicator.shape[0])]
+	max_col = max(col_lengths)
+	ans = [i for i in range(indicator.shape[0]) if col_lengths[i]==max_col][0]
+	# print("biggestCol:", ans)
+	return ans
+
+# def makeSymmetry2(indicator):
+# 	indicator = np.array(indicator)
+# 	possibilities = []
+# 	rows = 
+# 	for i in range(indicator.shape[0]):
+
+
+def makeSymmetry(indicator):
+	"""
+	takes in indicator array, returns string
+	"""
+	# nicePrint(indicator)
+	indicator = getIndicatorArray(indicator)
+	indicator = np.array(indicator)
+	# print(indicator.sum())
+	if indicator.sum() == 0: return
+	bot_row = 0
+	left_col = 0
+	indicator_len = indicator.shape[1]
+	while bot_row < indicator_len:
+		# print("bot_row", bot_row)
+		# print("indicator before row swap:", indicator)
+		# print("indicator in makeSym from 1 up:", indicator[:][1:])
+		# print("indicator in makeSym from bot_row up:", indicator[:, bot_row:])
+		big_row_ind = bot_row + biggestRow(indicator[:, bot_row:])
+		# print(indicator[:][big_row_ind])
+		temp = np.copy(indicator[:, big_row_ind])
+		# temp = indicator[:][big_row_ind]
+		indicator[:, big_row_ind] = indicator[:, bot_row]
+		indicator[:, bot_row] = temp
+		threshold = bot_row + 1
+		# print("temp:", temp)
+		# print("indicator after row swap:", indicator)
+		#gravity that boi
+		bottom_row_has_one = [i for i in range(left_col, NUM_COLS) if indicator[i, bot_row] == 1]
+		curr_col = left_col
+		while curr_col < NUM_COLS - 1 and bottom_row_has_one != []:
+			if indicator[curr_col, bot_row] != 1:
+				temp = np.copy(indicator[curr_col])
+				indicator[curr_col] = indicator[bottom_row_has_one[0]]
+				indicator[bottom_row_has_one[0]] = temp
+				# print("in between:", indicator, curr_col, left_col, bottom_row_has_one)
+			bottom_row_has_one.pop(0)
+			curr_col += 1
+		# print("indicator after row gravity:", indicator)
+		# for i in range(indicator_len):
+		# 	print(indicator[i][bot_row])
+		arr = [i for i in range(indicator_len) if indicator[i][bot_row] == 1]
+		if arr == []: break
+		right_col = max(arr)
+		# print("right_col", right_col)
+		while left_col < right_col:# and indicator[left_col][bot_row] == 1:
+			big_col_ind = left_col + biggestCol(indicator[left_col:right_col + 1, bot_row:]) #biggestCol(indicator[left_col:right_col + 1][threshold:])
+			temp = np.copy(indicator[left_col])
+			indicator[left_col] = indicator[big_col_ind]
+			indicator[big_col_ind] = temp
+			#gravity that boi
+			left_col_has_one = [i for i in range(threshold, NUM_ROWS) if indicator[left_col][i] == 1]
+			curr_row = threshold
+			while curr_row < NUM_ROWS - 1 and left_col_has_one != []:
+				if indicator[left_col][curr_row] != 1:
+					temp = np.copy(indicator[:, curr_row])
+					indicator[:, curr_row] = indicator[:, left_col_has_one[0]] #indicator[:][left_col_has_one[0]]
+					indicator[:, left_col_has_one[0]] = temp
+				left_col_has_one.pop(0)
+				curr_row += 1
+			threshold += 1
+			left_col += 1
+			# if threshold >= right_col:
+			# 	left_col = right_col + 1
+			# 	break
+		left_col += 1
+		bot_row = threshold
+	return stringifyIndicator(indicator.tolist())
+
+
+
+
+	# nicePrint(indicator)
+	# bot_row = 0
+	# left_col = 0
+	# while bot_row < len(indicator):
+	# 	print(bot_row)
+	# 	print("indicator in makeSym:", indicator[:])
+	# 	print("indicator in makeSym from 1 up:", indicator[:][1:])
+	# 	print("indicator in makeSym from bot_row up:", getRows(indicator, bot_row))
+	# 	big_row_ind = bot_row + biggestRow(getRows(indicator, bot_row))
+	# 	# print(indicator[:][big_row_ind])
+	# 	temp = indicator[:][big_row_ind]
+	# 	indicator[:][big_row_ind] = indicator[:][bot_row]
+	# 	indicator[:][bot_row] = temp
+	# 	threshold = bot_row + 1
+	# 	right_col = max([i for i in range(len(indicator)) if indicator[i][bot_row] == 1])
+	# 	# unsorted = [i for i in range(left_col, len(indicator)) if indicator[i][bot_row] == 1]
+	# 	while left_col < right_col and indicator[left_col][bot_row] == 1:
+	# 		big_col_ind = left_col + biggestCol(getRows(indicator[left_col:right_col + 1], threshold)) #biggestCol(indicator[left_col:right_col + 1][threshold:])
+	# 		temp = indicator[left_col]
+	# 		indicator[left_col] = indicator[big_col_ind]
+	# 		indicator[big_col_ind] = temp
+	# 		#gravity that boi
+	# 		left_col_has_one = [i for i in range(threshold, NUM_ROWS) if indicator[left_col][i] == 1]
+	# 		curr_row = threshold
+	# 		while curr_row < NUM_ROWS - 1 and left_col_has_one != []:
+	# 			if indicator[left_col][curr_row] != 1:
+	# 				temp = indicator[:][curr_row]
+	# 				indicator[:][curr_row] = getRow(indicator, left_col_has_one[0]) #indicator[:][left_col_has_one[0]]
+	# 				indicator[:][left_col_has_one[0]] = temp
+	# 				left_col_has_one.pop(0)
+	# 			threshold += 1
+	# 			curr_row += 1
+	# 		left_col += 1
+	# 		if threshold >= right_col:
+	# 			left_col = right_col + 1
+	# 			break
+	# 	bot_row = threshold
+	# nicePrint(indicator)
+
+
+def nicePrint(arr):
+	for col in arr:
+		print(col)
+
+"""
+basic computer turn
+"""
 
 def circleCount(circles):
 	count = 0
@@ -65,57 +226,134 @@ def doTurn(circles, turn):
 
 winForCurrPlayer = {} # dict from board to (first player winning, winning move if True)
 
-def toString(circles):
+def stringifyIndicator(indicator):
+	ans = str(NUM_ROWS)
+	for col in indicator:
+		for circ in col:
+			ans += str(circ)
+	return ans
+
+
+def stringify(circles):
 	ans = str(NUM_ROWS)
 	for col in getIndicatorArray(circles):
 		for circ in col:
 			ans += str(circ)
 	return ans
 
+counter = 0
+
 def performComputerTurnHelper(circles):
+	# global counter
+	# counter += 1
 	# winForCurrPlayer.clear() #uncomment this to stop memoization
 	comp_moves = getLegalMoves(circles)
-	circle_indicator = toString(circles)
-	if circle_indicator in winForCurrPlayer.keys():
-		# print(winForCurrPlayer[circle_indicator])
-		return winForCurrPlayer[circle_indicator]
-	# print(circleCount(circles))
-	# print("Current Board:", circles)
-	# print("Possible moves:", comp_moves)	
+	original_board = stringify(circles)
+	if not winForCurrPlayer.get(original_board, True):
+		return False, None
 	for move in comp_moves: #if this loop runs 0 times, no legal moves for computer (so loss)
 		newBoard = doTurn(circles, move)
+		circle_indicator = stringify(newBoard)
+		if circle_indicator in winForCurrPlayer.keys():
+			if not winForCurrPlayer[circle_indicator]:
+				return True, move
+			else:
+				continue
 		human_moves = getLegalMoves(newBoard)
-		# print("board for human:", newBoard)
-		# print("legal human moves:", human_moves)
 		alwaysWins = True
 		for human in human_moves: # if no human moves, then it's a win for the computer
+			new_indicator = stringify(newBoard)
+			if new_indicator in winForCurrPlayer.keys(): #board after human move already solved
+				isWin = winForCurrPlayer[new_indicator]
+				if isWin:
+					winForCurrPlayer[original_board] = True
+					return True, move
+				else:
+					continue
 			human_board = doTurn(newBoard, human)
 			isWin, _ = performComputerTurnHelper(human_board)
-			# print("examining human move:", human)
-			# print("results in win for computer:", isWin)
-			# print("on board", human_board)
 			if not isWin:
 				alwaysWins = False
 				break
 		if alwaysWins:
-			winForCurrPlayer[circle_indicator] = (True, move)
+			winForCurrPlayer[original_board] = True
 			return True, move
+	winForCurrPlayer[original_board] = False
+	return False, None
+
+# counter = 0
+
+def performComputerTurnHelper2(circles):
+	# global counter
+	# counter += 1
+	# winForCurrPlayer.clear() #uncomment this to stop memoization
+	comp_moves = getLegalMoves(circles)
+	circle_indicator = stringify(circles)
+	if circle_indicator in winForCurrPlayer.keys():
+		# print(winForCurrPlayer[circle_indicator])
+		return winForCurrPlayer[circle_indicator]
+	for move in comp_moves: #if this loop runs 0 times, no legal moves for computer (so loss)
+		newBoard = doTurn(circles, move)
+		human_moves = getLegalMoves(newBoard)
+		alwaysWins = True
+		if stringify(newBoard) in winForCurrPlayer: #board after human move already solved
+			isWin, _ = winForCurrPlayer[stringify(newBoard)]
+			if not isWin:
+				winForCurrPlayer[circle_indicator] = (True, move)
+				return True, move
+			else:
+				continue
+		else:
+			for human in human_moves: # if no human moves, then it's a win for the computer
+				human_board = doTurn(newBoard, human)
+				lastBoard = stringify(human_board)
+				isWin = winForCurrPlayer.get(lastBoard, performComputerTurnHelper2(human_board))
+				if not isWin:
+					alwaysWins = False
+					break
+			if alwaysWins:
+				winForCurrPlayer[circle_indicator] = (True, move)
+				return True, move
 	winForCurrPlayer[circle_indicator] = (False, None)
 	return False, None
 
+def performComputerTurnHelper3(circles):
+	circle_indicator = makeSymmetry(circles)
+	if circle_indicator in winForCurrPlayer.keys():
+		return winForCurrPlayer[circle_indicator]
+	legal_moves = getLegalMoves(circles)
+	for move in legal_moves:
+		newBoard = doTurn(circles, move)
+		if not performComputerTurnHelper3(newBoard):
+			winForCurrPlayer[circle_indicator] = True
+			return True
+	winForCurrPlayer[circle_indicator] = False
+	return False
+
 def performComputerTurn(circles, winner_predict, win):
+	start_time = time.time()
 	loading = Text(Point(NUM_COLS * SQUARE_WIDTH + 50, 140), "Thinking...")
 	loading.draw(win)
-	# for legalMove in getLegalMoves(circles):
-	# 	print(legalMove)
-	isWin, moves = performComputerTurnHelper(circles)
+	for move in getLegalMoves(circles):
+		newCircles = doTurn(circles, move)
+		isWin = not performComputerTurnHelper3(newCircles)
+		if isWin:
+			loading.undraw()
+			winner_predict.setText("Computer will win!")
+			print("--- %s seconds ---" % (time.time() - start_time))
+			return True, move
 	loading.undraw()
-	if isWin:
-		winner_predict.setText("Computer will win!")
-	else:
-		winner_predict.setText("Player will win!")	
-	return isWin, moves
+	# if isWin:
+		# winner_predict.setText("Computer will win!")
+	# else:
+	winner_predict.setText("Player will win!")
+	print("--- %s seconds ---" % (time.time() - start_time))
+	# print(counter)
+	return False, None
 
+"""
+general gameplay
+"""
 
 def playGame():
 	win = GraphWin("Nim Player Interface", SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -124,18 +362,23 @@ def playGame():
 	endTurn = Rectangle(Point(NUM_COLS * SQUARE_WIDTH + 10, 90), Point(NUM_COLS * SQUARE_WIDTH + 90, 10))
 	endTurn.setFill("red")
 	endTurn.draw(win)
+	exitGame = Rectangle(Point(NUM_COLS * SQUARE_WIDTH + 20, 200), Point(NUM_COLS * SQUARE_WIDTH + 80, 160))
+	exitGame.setFill("yellow")
+	exitGame.draw(win)
+	exit_text = Text(Point(NUM_COLS * SQUARE_WIDTH + 50, 180), "Exit")
+	exit_text.draw(win)
 	player_text = Text(Point(NUM_COLS * SQUARE_WIDTH + 50, 50), "Player's turn")
 	player_text.draw(win)
 	winner_predict = Text(Point(NUM_COLS * SQUARE_WIDTH + 50, 110), "Who's gonna win?")
 	winner_predict.draw(win)
 	player_1_wins = 0
 	player_2_wins = 0
-	player_1_win_text = Text(Point(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 67), "Player wins: 0")
+	player_1_win_text = Text(Point(NUM_COLS * SQUARE_WIDTH // 2, SCREEN_HEIGHT - 67), "Player wins: 0")
 	player_1_win_text.draw(win)
-	player_2_win_text = Text(Point(SCREEN_WIDTH // 2, SCREEN_HEIGHT - 33), "Computer wins: 0")
+	player_2_win_text = Text(Point(NUM_COLS * SQUARE_WIDTH // 2, SCREEN_HEIGHT - 33), "Computer wins: 0")
 	player_2_win_text.draw(win)
 	while(True):
-		winForCurrPlayer.clear()
+		# winForCurrPlayer.clear()
 		winner_predict.setText("Who will win?")
 		circles = createCircles(win)
 		player_1_turn = True
@@ -144,6 +387,7 @@ def playGame():
 		tiles_removed = 0
 		row_selection = -1
 		col_selection = -1
+		# makeSymmetry(getIndicatorArray(circles))
 		if COMPUTER_FIRST:
 			comp_win, moves = performComputerTurn(circles, winner_predict, win)
 			if comp_win:
@@ -159,7 +403,7 @@ def playGame():
 				circles[random_col].remove(toRemove)
 				toRemove.undraw()
 			if circles != [[] for _ in range(NUM_COLS)]:
-				player_1_turn = True
+				player_1_turn = True 
 				player_text.setText("Player's turn" if player_1_turn else "Computer's turn")
 
 		while circles != [[] for i in range(NUM_COLS)]:
@@ -174,6 +418,7 @@ def playGame():
 					player_text.setText("Player's turn" if player_1_turn else "Computer's turn")
 					row_selection = -1
 					col_selection = -1
+					# makeSymmetry(getIndicatorArray(circles))
 					comp_win, moves = performComputerTurn(circles, winner_predict, win)
 					if comp_win:
 						for move in moves:
@@ -191,7 +436,8 @@ def playGame():
 					if circles != [[] for _ in range(NUM_COLS)]:
 						player_1_turn = True
 						player_text.setText("Player's turn" if player_1_turn else "Computer's turn")
-
+			elif clickedExit(click):
+				return
 			else:
 				# print(len(circles))
 				for column in range(len(circles)):
@@ -223,8 +469,14 @@ def playGame():
 	win.getMouse()
 	win.close()
 
+"""
+various Helper Methods
+"""
+
 def circToArrayIndex(circle):
 	return int((circle.getCenter().getY())/SQUARE_WIDTH), int((circle.getCenter().getX() - SQUARE_WIDTH/2.0)/SQUARE_WIDTH)
+
+# def circArrayToMatrixIndex()
 
 def arrayIndexToCirc(row, col, circles):
 	for entry in circles[col]:
@@ -306,15 +558,11 @@ def getLegalMoves(circles):
 	# time.sleep(1)
 	return output_final
 
-	#create indicator array
-
-
-
-# def doTurn(circles, circles):
-
 
 if __name__ == "__main__":
 	playGame()
+
+
 
 
 
